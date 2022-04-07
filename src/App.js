c083c1ef100d8b38341ad69b5dc7ad0ec453099a
josef <josephsipiorski@gmail.com>
2022-04-07 18:15:24 -0500

    create new animal component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import AnimalList from './component/list'
+import AnimalCreate from './component/create';
 
 function App() {
   return (
     <div className="App">
+      <AnimalCreate/>
       <AnimalList/>
     </div>
   );
---
+++ src/component/create.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class AnimalCreate extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>This is Create new animal component</div>
+        )
+    }
+}
+
+export default AnimalCreate
